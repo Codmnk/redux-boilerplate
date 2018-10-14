5e5fc9306cc99a9038a4ef1aa8c403a8d0a2a380
Prem Acharya <prem.acharya@hotmail.com>
2018-10-14 22:20:05 +1100

    first commit

---
+++ src/actions/postAction.js
@@ -0,0 +1,13 @@
+import { FETCH_POSTS, NEW_POST } from "./types";
+import Posts from "../components/Posts";
+
+export const fetchPosts = () => dispatch => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(posts =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    );
+};
